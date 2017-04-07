2e1e7decc5c9591da3a2cfd8d90693e7ef16d93e
James Higgins <james.milan.higgins@gmail.com>
2017-04-07 13:12:32 -0700

    add and change js to make toppings push to array and if statements work based off of array length.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,17 @@
 /////////////////
 //Back-End Logic
 /////////////////
-function Pizza(name, size, topping, price) {
+function Pizza(name, size, toppings, price) {
   this.name = name;
   this.size = size;
-  this.topping = [];
-  // this.topping = [cheese, peperoni, sausage];
+  this.toppings = [];
   this.price = 0;
+  // this.topping = [cheese, peperoni, sausage];
   // this.topping = topping;
   // this.size = [medium, large, xl];
   // this.size = [1, 2, 3];
   // this.topping = [cheese, pepperoni, sausage];
 }
-
 Pizza.prototype.alterPrice = function() {
   if (this.size === "small") {
     this.price += 6;
@@ -26,43 +25,43 @@ Pizza.prototype.alterPrice = function() {
     else if (this.size === "xl") {
       this.price += 12;
     }
-    if ((this.topping.length === 1)) {
+    if ((this.toppings.length === 1)) {
       this.price += 1;
     }
-    else if (this.topping.length === 2) {
+    else if (this.toppings.length === 2) {
       this.price +=2;
     }
-    else if (this.topping.length === 3) {
+    else if (this.toppings.length === 3) {
       this.price +=3;
     }
-    else if (this.topping.length === 4) {
-      this.price +=5;
+    else if (this.toppings.length === 4) {
+      this.price +=4;
     }
     return this.price;
   }
-
-
-
-// }
-
 //////////////////
 //Front-End Logic
 //////////////////
 $(document).ready(function() {
    $("button[name=submit-button]").click(function(){
+    var newPizza = new Pizza(nameInput, pizzaSizeInput, toppings2, 0);
     var nameInput =  $("input[name=name]").val();
     var pizzaSizeInput = $("#pizza-size").val();
-    var toppingInput = $("input:checkbox[name=topping]:checked").val();
-    var newPizza = new Pizza(nameInput, pizzaSizeInput, toppingInput);
-   //
-  //  $("input:checkbox[name=topping]:checked").each(function (){
-  //    var toppingsInput = $(this).val();
-  //  });
-  //  var name = new Pizza(nameInput);
-  //  var size = new Pizza(pizzaSizeInput);
+    var toppings2 = $("input:checkbox[name=topping]:checked").each(function() {
+      var toppingInput = $(this).val();
+      newPizza.toppings.push(toppings2);
+      console.log(toppings2);
+    });
+    // console.log(toppings);
+    console.log(toppings2);
+    console.log(newPizza);
 
-  console.log(newPizza);
-  $("#hidden").append("<li>" +'Hi ' + nameInput + '. The cost of your pizza is $' + newPizza.alterPrice() + "</li>");
+    // var toppingInput = $("input:checkbox[name=topping]:checked").val();
+    // var toppingsInput = [];
+    // var test = $("input:checkbox[name=topping]:checked").each(function (){
+    //   var toppingInput = $(this).val();
+    //     console.log(toppingInput);
+    $("#hidden").append("<li>" +'Hi ' + nameInput + '. The cost of your pizza is $' + newPizza.alterPrice() + "</li>");
   newPizza.alterPrice();
   });
 });
