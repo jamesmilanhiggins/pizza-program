27fd480da20bb9dcc48ea9c1f64c00f1f89eb53c
James Higgins <james.milan.higgins@gmail.com>
2017-04-07 13:43:49 -0700

    change order of newPizza.alterprice and var newPizza = because it was affecting the price and order

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,10 @@ function Pizza(name, size, toppings, price) {
   this.size = size;
   this.toppings = [];
   this.price = 0;
-  // this.topping = [cheese, peperoni, sausage];
-  // this.topping = topping;
-  // this.size = [medium, large, xl];
-  // this.size = [1, 2, 3];
-  // this.topping = [cheese, pepperoni, sausage];
 }
 Pizza.prototype.alterPrice = function() {
-  if (this.size === "small") {
-    this.price += 6;
+  if ((this.size === "small") && (this.toppings.length <= 5)) {
+    this.price += 6000;
   }
     else if (this.size === "medium") {
       this.price += 8;
@@ -25,7 +20,7 @@ Pizza.prototype.alterPrice = function() {
     else if (this.size === "xl") {
       this.price += 12;
     }
-    if ((this.toppings.length === 1)) {
+    else if((this.toppings.length === 1)) {
       this.price += 1;
     }
     else if (this.toppings.length === 2) {
@@ -37,6 +32,12 @@ Pizza.prototype.alterPrice = function() {
     else if (this.toppings.length === 4) {
       this.price +=4;
     }
+    else if (this.toppings.length === 5) {
+      this.price +=5;
+    }
+    else if (this.toppings.length === 6) {
+      this.price +=6;
+    }
     return this.price;
   }
 //////////////////
@@ -44,24 +45,17 @@ Pizza.prototype.alterPrice = function() {
 //////////////////
 $(document).ready(function() {
    $("button[name=submit-button]").click(function(){
-    var newPizza = new Pizza(nameInput, pizzaSizeInput, toppings2, 0);
     var nameInput =  $("input[name=name]").val();
     var pizzaSizeInput = $("#pizza-size").val();
-    var toppings2 = $("input:checkbox[name=topping]:checked").each(function() {
-      var toppingInput = $(this).val();
-      newPizza.toppings.push(toppings2);
-      console.log(toppings2);
-    });
-    // console.log(toppings);
-    console.log(toppings2);
-    console.log(newPizza);
+    var newPizza =  new Pizza(nameInput, pizzaSizeInput, toppingsInput);
 
-    // var toppingInput = $("input:checkbox[name=topping]:checked").val();
-    // var toppingsInput = [];
-    // var test = $("input:checkbox[name=topping]:checked").each(function (){
-    //   var toppingInput = $(this).val();
-    //     console.log(toppingInput);
+    var toppingsInput = $("input:checkbox[name=topping]:checked").each(function() {
+      var toppingInput = $(this).val();
+      newPizza.toppings.push(toppingsInput);
+    });
     $("#hidden").append("<li>" +'Hi ' + nameInput + '. The cost of your pizza is $' + newPizza.alterPrice() + "</li>");
-  newPizza.alterPrice();
+    console.log(newPizza);
+    newPizza.alterPrice();
+
   });
 });
